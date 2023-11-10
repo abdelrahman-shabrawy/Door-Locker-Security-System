816020672ab04c313c9b614a02abb97b8f1b9a04
Abdelrahman Shabrawy <52890449+abdelrahman-shabrawy@users.noreply.github.com>
2023-11-10 16:13:50 +0200

    Delete Final project proteus simulation/mc_1 directory

--- Final project proteus simulation/mc_1/Release/MCAL/GPIO/gpio.d
+++
@@ -1,9 +0,0 @@
-MCAL/GPIO/gpio.o MCAL/GPIO/gpio.o: ../MCAL/GPIO/gpio.c \
-  ../MCAL/GPIO/gpio.h ../MCAL/GPIO/../../LIBRARIES/std_types.h \
-  ../MCAL/GPIO/../../LIBRARIES/common_macros.h
-
-../MCAL/GPIO/gpio.h:
-
-../MCAL/GPIO/../../LIBRARIES/std_types.h:
-
-../MCAL/GPIO/../../LIBRARIES/common_macros.h:
--- Final project proteus simulation/mc_1/Release/MCAL/UART/uart.d
+++
@@ -1,9 +0,0 @@
-MCAL/UART/uart.o MCAL/UART/uart.o: ../MCAL/UART/uart.c \
-  ../MCAL/UART/uart.h ../MCAL/UART/../../LIBRARIES/std_types.h \
-  ../MCAL/UART/../../LIBRARIES/common_macros.h
-
-../MCAL/UART/uart.h:
-
-../MCAL/UART/../../LIBRARIES/std_types.h:
-
-../MCAL/UART/../../LIBRARIES/common_macros.h:
