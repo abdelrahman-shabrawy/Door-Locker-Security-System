250f0615551484b8b5d8408e2be8b45972fea2d8
Abdelrahman Shabrawy <52890449+abdelrahman-shabrawy@users.noreply.github.com>
2023-11-10 16:16:20 +0200

    Add files via upload

---
+++ mc_1/Debug/MCAL/TIMER/timer.d
@@ -0,0 +1,9 @@
+MCAL/TIMER/timer.o MCAL/TIMER/timer.o: ../MCAL/TIMER/timer.c \
+  ../MCAL/TIMER/timer.h ../MCAL/TIMER/../../LIBRARIES/std_types.h \
+  ../MCAL/TIMER/../../LIBRARIES/common_macros.h
+
+../MCAL/TIMER/timer.h:
+
+../MCAL/TIMER/../../LIBRARIES/std_types.h:
+
+../MCAL/TIMER/../../LIBRARIES/common_macros.h:
